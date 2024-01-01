3ece7f51db8db21d46d77d0cbd0ecfa5b65b8958
taras.chmeruk <taras.chmeruk@gameloft.com>
2024-01-01 18:46:46 +0200

    bla

---
+++ src/main/java/com/webfluxpattern/WebfluxPatternApplication.java
@@ -0,0 +1,13 @@
+package com.webfluxpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webfluxpattern/WebfluxPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webfluxpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
